ab7bbe1e2a952894e362efd7e13a9b607f8f913b
Beatriz Angelita <beatrizelzi@gmail.com>
2023-06-01 12:41:58 -0300

    Deploy

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://blogpessoal-d7lo.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
     export const cadastroUsuario = async(url: string, dados: object, setDado: any) => { 
